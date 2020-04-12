971b3c0391dfcd026f7e0751f772a5725495db85
Indra Mohan <indrathota1993@gmail.com>
2020-04-12 20:52:57 +0530

    Implemented custom excel reading and table implemented

--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
-console.log("Indra Mohan Thota");
 var ExcelToJSON = function () {
 
-    this.parseExcel = function (file) {
+    this.parseExcel = function (file, week) {
         var reader = new FileReader();
-
+        console.log(week);
         reader.onload = function (e) {
-            var data = e.target.result;
-            var workbook = XLSX.read(data, {
+            var res = e.target.result;
+            var workbook = XLSX.read(res, {
                 type: 'binary'
             });
-            workbook.SheetNames.forEach(function (sheetName) {
-                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-                var json_object = JSON.stringify(XL_row_object);
-                let labels = [], data = [];
-                XL_row_object.forEach(pair => {
-                    labels.push(pair.Name);
-                    data.push(pair.Marks);
-                });
-                addData(chart, labels, data);
-            })
+            var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets['Week-' + week]);
+            let data = [];
+            XL_row_object.forEach(row => {
+                let active = 0, inactive = 0, error = 0;
+                for (let key in row) {
+                    if (row[key] === 'Active') {
+                        active++;
+                    }
+                    if (row[key] === 'Inactive') {
+                        inactive++;
+                    } if (row[key] === 'Error') {
+                        error++;
+                    }
+                }
+                row['Active'] = active;
+                row['Inactive'] = inactive;
+                row['Error'] = error;
+                data.push(row);
+            });
+            let groupedData = _.groupBy(data, 'Environment');
+            createHTMLTable(groupedData);
+            // addData(chart, labels, data);
         };
 
         reader.onerror = function (ex) {
@@ -29,8 +40,51 @@ var ExcelToJSON = function () {
     };
 };
 
+function createHTMLTable(data) {
+    let tableContainer = document.getElementById('table');
+    let wrapper = document.createElement('div');
+    wrapper.classList.add('table-responsive-sm');
+    let table = document.createElement('table');
+    table.classList.add('table', 'table-hover');
+    table.innerHTML = `
+        <thead>
+            <tr>
+                <th scope="col">Environment</th>
+                <th scope="col">API</th>
+                <th scope="col">API Endpoint</th>
+                <th scope="col">Active</th>
+                <th scope="col">Inactive</th>
+                <th scope="col">Error</th>
+            </tr>
+        <thead>
+    `;
+    wrapper.appendChild(table);
+    tableContainer.appendChild(wrapper);
+    let tbody = document.createElement('tbody');
+    for (let key in data) {
+        let tr = document.createElement('tr');
+        tr.classList.add('table-active');
+        tr.innerHTML = `<td colspan='6'>${key}</td>`;
+        tbody.appendChild(tr);
+        if (Array.isArray(data[key])) {
+            for (let obj of data[key]) {
+                let trt = document.createElement('tr');
+                trt.innerHTML = `
+                    <td>${obj.Environment}</td>
+                    <td>${obj.API}</td>
+                    <td>${obj.API_ENDPOINT}</td>
+                    <td>${obj.Active}</td>
+                    <td>${obj.Inactive}</td>
+                    <td>${obj.Error}</td>
+                `;
+                tbody.appendChild(trt);
+            }
+        }
+    }
+    table.appendChild(tbody);
+}
+
 function addData(chart, label, data) {
-    console.log(chart);
     chart.data.labels = label;
     chart.data.datasets.forEach((dataset) => {
         dataset.data = data;
@@ -39,24 +93,24 @@ function addData(chart, label, data) {
 }
 
 var customFile = document.getElementById('customFile');
-console.log('Custom File', customFile);
+var weekSelected = document.getElementById('week');
 customFile.addEventListener('change', (evt) => {
     console.log(evt);
     var files = evt.target.files;
     var xl2json = new ExcelToJSON();
-    xl2json.parseExcel(files[0]);
+    xl2json.parseExcel(files[0], weekSelected.value);
 });
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Marks',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-    options: {}
-});
+// var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'Marks',
+//             backgroundColor: 'rgb(255, 99, 132)',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [0, 10, 5, 2, 20, 30, 45]
+//         }]
+//     },
+//     options: {}
+// });
